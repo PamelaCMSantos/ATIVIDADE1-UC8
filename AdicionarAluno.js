dbf99da9918aade64669baffd96a30a1950a5982
Pamela Santos <pamelacmagalhaes30@gmail.com>
2022-08-19 19:53:08 -0300

    first commit

---
+++ AdicionarAluno.js
@@ -0,0 +1,29 @@
+let DataAtual = new Date (22, 8, 19);
+let DataDoEvento = new Date (22,9, 20)
+console.log (DataDoEvento)
+
+if (DataDoEvento > DataAtual) {console.log ("Cadastrar evento");}
+    else {console.log ("data inválida, cadastro não permitido");
+}
+
+console.log ("-----------");
+
+let IdadeDoParticipante = 18
+
+if (IdadeDoParticipante < 18) {
+    ("cadastro não permitido, idade inválida");
+} else (IdadedDoParticipante >= 18) {
+    console.log ("cadastro realizado");
+}
+
+console.log ("----------");
+
+let ListaDeParticipantes = ["Helena", "Jose", "Felipe", "Jessica", "Carol"];
+let QuantidadeDeEstudantes = ListaDeEstudantes.length;
+if (QuantidadeDeEstudantes > 100) {
+    ListaDeEstudantes.push("nome");
+console.log(ListaDeEstudantes;);
+} else {
+    console.log("não é possivel realizar cadastro pois o lime foi excedido");
+}
+
