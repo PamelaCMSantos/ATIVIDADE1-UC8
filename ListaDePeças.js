9ea2615048d372f83a0ee449d20cdf8ae19ae6cb
Pamela Santos <pamelacmagalhaes30@gmail.com>
2022-08-19 21:32:36 -0300

    atividade 2

---
+++ ListaDePeças.js
@@ -0,0 +1,34 @@
+const listadepecas = ["Motor", "pe", "para brisa"];
+
+let PesoPeca = 90
+
+if (PesoPeca > 100) {
+    console.log("cadastrando o produto;");
+} else  {
+    console.log("não foi possivel cadastrar: peso não alcançado");
+}
+
+console.log ("fim do cadastrado")
+
+let QtDePecas = 20
+
+if (QtDePecas < 10) {
+    console.log ("cadastrando o produto");
+} else {
+    console.log ("não foi possivel cadastrar: limite de caixas ultrapassado");
+}
+
+console.log ("fim do cadastro")
+
+
+for (nomepeca < listadepecas . length; nomepeca++);
+
+let nomepeca = 5;
+
+if (nomepeca . length < 3) {
+    console.log ("erro nome da peça");
+} else {
+    console.log ("cadastrando o produto");
+}
+
+console.log ("cadastro de nova peça concluido com sucesso")
